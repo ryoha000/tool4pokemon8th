c1aad2e696dc3b0e557ad881577b4dc23d3bac1d
ryoha000 <48485650+ryoha000@users.noreply.github.com>
2019-10-13 12:19:39 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,8 @@ const App: React.FC = () => {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>ryha</p>
+        <p>ryoha</p>
         <a
           className="App-link"
           href="https://reactjs.org"
